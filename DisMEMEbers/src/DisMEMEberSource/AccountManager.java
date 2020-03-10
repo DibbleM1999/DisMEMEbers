ee0f0e459ecca66240da7fc4756b4b78ef75017f
hridgeway <hridgeway@ATC256-26.ad.ssucet>
2020-03-10 17:00:20 -0400

    Shell program of the Account Manager

---
+++ DisMEMEbers/src/DisMEMEberSource/AccountManager.java
@@ -0,0 +1,47 @@
+package DisMEMEberSource;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.ArrayList;
+//import java.util.List;
+/**
+ *
+ * @author hridgeway
+ */
+public class AccountManager {
+    
+    protected int num_acc; //Account count
+    protected ArrayList<Integer> Userlist = new ArrayList<Integer>(); //list of users. Holds ints until a user class is made to fill the list.
+    
+    protected AccountManager()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            Userlist.add(num_acc);
+            num_acc++;
+        }
+    }
+    
+    public void add(int val)//finish when user class is made
+    {
+        
+    }
+    
+    public void remove(int index)//Same as add but removing
+    {
+        try
+        {
+            int i = Userlist.get(index);
+        }catch(ArrayIndexOutOfBoundsException e){System.out.println(e);}
+         System.out.println("Index is less than Zero or greater than size of array");
+    }
+    
+    public int getUser(int ID) //will return user once created
+    {
+        return this.Userlist.get(0);
+    }
+    
+}
