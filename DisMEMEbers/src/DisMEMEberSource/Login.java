a930f4a3eec56bd154a5cb64da673d71586ca3a8
galva <galva@146.85.185.87>
2020-03-10 16:42:47 -0400

    Login class created and bootstrap and jquery added

---
+++ DisMEMEbers/src/DisMEMEberSource/Login.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DisMEMEberSource;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ezekiel and Dan
+ */
+public class Login {
+    static ArrayList<Account> Accounts = new ArrayList<Account>();
+    
+    public Login(String username, String password)
+    {
+        Account NewAccount = Account(username, password);
+        
+    }
+    
+}
