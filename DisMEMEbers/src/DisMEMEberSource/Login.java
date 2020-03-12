5566b70798229a920bad48f95a7703f68c70c194
dfred <dfred@146.85.185.52>
2020-03-12 16:35:44 -0400

    worked on login class more. added servlet stuff. need to update taking data from account manager

--- DisMEMEbers/src/DisMEMEberSource/Login.java
+++ DisMEMEbers/src/DisMEMEberSource/Login.java
@@ -4,24 +4,37 @@
  * and open the template in the editor.
  */
 package DisMEMEberSource;
-
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 
 /**
  *
  * @author Ezekiel and Dan
  */
-public class Login {
-    static ArrayList<Account> Accounts = new ArrayList<Account>();
+@WebServlet(urlPatterns={"/Login"})
+public class Login extends HttpServlet
+{
     
-    public Login(String username, String password)
-    {
-        
-        Account NewAccount = new Account(username, password);
-        Accounts.add(NewAccount);
-        
-        
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+        resp.setContentType("text/plain");
+        var pw = resp.getWriter();
+        var iUser = req.getParameter("user");
+        var iPass = req.getParameter("pass");
+        for (int i = 0; i < Account.next_UID; i++){
+            var user = "Account.class.getUsername();";
+            var pass = "Account.class.getPassword();";
+            if( iUser.equals(user) || iPass.equals(pass)){
+                var sess = req.getSession();
+                pw.printf("Logged in "+user);
+            } 
+            else {
+                pw.printf("Invalid User/pass");
+        }
+        }
     }
-    
-    
+
 }
