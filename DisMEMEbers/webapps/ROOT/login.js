3d894f5bc2389d402070d3de4ed11865033b023c
DibbleM1999 <47530229+DibbleM1999@users.noreply.github.com>
2020-03-24 16:55:26 -0400

    New Merge

---
+++ DisMEMEbers/webapps/ROOT/login.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function Login(){
+    // Will eventually want to redirect us to our login servlet
+    document.location="https://www.google.com/search?q=cat+memes&rlz=1C1NDCM_enUS808US808&sxsrf=ALeKk02wJjq_YUaakCWdMUdrynxixHW6zA:1584045370985&source=lnms&tbm=isch&sa=X&ved=2ahUKEwii5JTt5JXoAhVaWs0KHcpCAvQQ_AUoAXoECA8QAw";
+}
+
+
+
