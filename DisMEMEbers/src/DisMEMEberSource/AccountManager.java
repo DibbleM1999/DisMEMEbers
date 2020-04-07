afd89789823c54ee0399211202da947a70c562bf
DeBbb <DeBbb@206.21.94.152>
2020-04-07 17:37:19 -0400

    Added following features to AccountManager - Holden/Mason

--- DisMEMEbers/src/DisMEMEberSource/AccountManager.java
+++ DisMEMEbers/src/DisMEMEberSource/AccountManager.java
@@ -47,6 +47,14 @@ public class AccountManager {
                   + "avatar blob"
                   + ")");
             stmt.executeUpdate();
+            
+            
+            stmt = conn.prepareStatement(
+                    "create table follow ("
+                  + "fuid integer references users(uid), "
+                  + "feduid integer references users(uid), "
+                  + ")");
+            stmt.executeUpdate();
         }
         catch(Exception e)
         {
@@ -157,10 +165,120 @@ public class AccountManager {
         return false;
     }
     
+    public boolean name_check(String username)
+    {
+        for(int i=0; i<Userlist.size();i++)
+        {
+            if(Userlist.get(i).getUsername() == username)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean email_check(String mail)
+    {
+        for(int i=0; i<Userlist.size();i++)
+        {
+            if(Userlist.get(i).getEmail() == mail)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
     public void Clear() //KILL EVERYONE
     {
         Userlist.get(0).resetUID();
         Userlist.clear();
     }
     
+    
+    public String userRelationship(String user, String other, boolean following) throws Exception //Will return the String of the followed user. -1 if none.
+    {                                               // True for following, false for unfollowing
+  
+        try(var conn = java.sql.DriverManager.getConnection("jdbc:derby:disMEMEber_db.sql"))
+        {
+            if (following)
+            {
+                var stmt = conn.prepareStatement("select fuid from follow where fuid=? and feduid=?");
+                stmt.setString(1, user);
+                stmt.setString(2, other);
+                var result = stmt.executeQuery();
+                if (result != null)
+                {
+                    return "Already following";
+                }
+                else
+                {
+            
+                    stmt = conn.prepareStatement("insert into follow "
+                            + "(fuid, feduid) "
+                            + "values (?, ?)");
+                    stmt.setString(1, user);
+                    stmt.setString(2, other);
+                    stmt.executeUpdate();
+                    return other;
+                }
+            }
+            else
+            {
+                var stmt = conn.prepareStatement("select fuid from follow where fuid=? and feduid=?");
+                stmt.setString(1, user);
+                stmt.setString(2, other);
+                var result = stmt.executeQuery();
+                if (result != null)
+                {
+                    return "Not already following";
+                }
+                else
+                {
+            
+                    stmt = conn.prepareStatement("delete from follow where fuid=? and feduid=?");
+                    stmt.setString(1, user);
+                    stmt.setString(2, other);
+                    stmt.executeUpdate();
+                    return other;
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            throw(e);
+        }
+    }
+    
+    public ArrayList<String> seeWhosFollowing(String user) throws Exception // Returns who's following the user
+    {
+        
+        try(var conn = java.sql.DriverManager.getConnection("jdbc:derby:disMEMEber_db.sql"))
+        {
+                var stmt = conn.prepareStatement("select fuid from follow where feduid=?");
+                stmt.setString(1, user);
+                var result = stmt.executeQuery();
+                return (ArrayList<String>) result;
+        }
+        catch(Exception e)
+        {
+            throw(e);
+        }
+    }
+    
+    public ArrayList<String> seeWhoTheyFollowing(String user) throws Exception  // Returns who user is following
+    {
+        
+        try(var conn = java.sql.DriverManager.getConnection("jdbc:derby:disMEMEber_db.sql"))
+        {
+                var stmt = conn.prepareStatement("select feduid from follow where fuid=?");
+                stmt.setString(1, user);
+                var result = stmt.executeQuery();
+                return (ArrayList<String>) result;
+        }
+        catch(Exception e)
+        {
+            throw(e);
+        }
+    }
 }
