69c947e8a6094353ad009f15703cd91339692964
Kyle Johnson <Brumous101@gmail.com>
2020-04-09 19:25:51 -0400

    Brice, Dion, and Kyle PostManager class and Post class. Initial setup of .gitignore to ignore the jetty and derby folders

--- DisMEMEbers/src/DisMEMEberSource/PostManager.java
+++ DisMEMEbers/src/DisMEMEberSource/PostManager.java
@@ -5,34 +5,43 @@
  */
 package DisMEMEberSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 /**
  *
- * @author Brice
+ * @author Brice, Kyle, and Dion
  */
 
 public class PostManager {
     public static Connection connection;
-    public PostManager instance;
+    //public PostManager instance;
+    //public static Post instance = new Post();
     public void PostManager(){
-        connection = main();
+        connection = setupTable();
     }
     
-    public static final String JDBC_URL = "jdbc:derby:zadb;create=true";
-    public static Connection main() {
-       try{
-            Connection connection = DriverManager.getConnection(JDBC_URL);
-            connection.createStatement().execute("create table Post(userID int, postID int, text varchar(20), imageLocation varchar(20),date varchar(20)");
-            return connection;
-      } catch(Exception e){ 
-           throw new RuntimeException(e);
-    }
+    private static final Connection JDBC_URL = DataBase.getInstance();
+    
+    public static Connection setupTable() 
+    {
+            try
+            {
+                connection.createStatement().execute("create table Post(userID int, postID int, text varchar(20), imageLocation varchar(20),date varchar(20)");
+                return connection;
+            } 
+            catch(SQLException e)
+            { 
+                throw new RuntimeException(e);
+            }
     }
     public static void createPost(int userID, int postID,char text,char imageLocation,char date) throws SQLException{
                   //use post class / create post class instead of what is below 
-        connection.createStatement().execute("insert into Post values"+userID+postID+text+imageLocation+date);
+        connection.createStatement().execute(
+                "insert into Post values"+
+                Post.instance.getUserID()+
+                Post.instance.getPostID()+
+                Post.instance.getText()+
+                Post.instance.getImageLocation()+
+                Post.instance.getDate());
        
     } 
 }
