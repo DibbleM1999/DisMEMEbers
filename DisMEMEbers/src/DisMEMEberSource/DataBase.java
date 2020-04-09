6f68d1e85e82dc3b2a1c4e9b3a6a45bf8964ae4c
TehJellyLord <dionpcullum29@gmail.com>
2020-04-09 16:32:05 -0400

    Created a Database singleton

--- DisMEMEbers/src/DisMEMEberSource/DataBase.java
+++ DisMEMEbers/src/DisMEMEberSource/DataBase.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package DisMEMEberSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  *
  * @author nickb
  */
 public class DataBase {
+    private static Connection conn;
+    public static final String JDBC_URL = "jdbc:derby:disMEMEber_db.sql;create=true";
     
+    static Connection getInstance(){
+       if( conn == null){
+           try{conn = DriverManager.getConnection(JDBC_URL);}
+           catch(Exception e){
+               throw new RuntimeException(e);
+           }
+       }
+       return conn;
+    }
 }
