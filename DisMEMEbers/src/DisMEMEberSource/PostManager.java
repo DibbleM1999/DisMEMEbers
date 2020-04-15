92db08f4fc8a082566dce86b5eda6cee1dd95651
TehJellyLord <dionpcullum29@gmail.com>
2020-04-15 14:37:00 -0400

    More tweaks to PostManager. Try-Catch statements added where needed.

--- DisMEMEbers/src/DisMEMEberSource/PostManager.java
+++ DisMEMEbers/src/DisMEMEberSource/PostManager.java
@@ -4,32 +4,51 @@
  * and open the template in the editor.
  */
 package DisMEMEberSource;
+import static DisMEMEberSource.DataBase.JDBC_URL;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Brice, Kyle, and Dion
  */
 
 public class PostManager {
+    public static Connection connection;
     //public PostManager instance;
     //public static Post instance = new Post();
     public void PostManager(){
+        connection = getConnection();
         setupTable();
     }
     
-    private static final Connection JDBC_URL = DataBase.getInstance();
+    //private static final Connection JDBC_URL = DataBase.getInstance();
+    private static Connection getConnection(){
+        if( connection == null){
+           try{connection = DataBase.getInstance();}
+           catch(Exception e){
+               throw new RuntimeException(e);
+           }
+       }
+       return connection;
+    }
     
     public static void setupTable() 
     {
-            var stmt = JDBC_URL.prepareStatement(
-            "create table posts ("
-              + "uid integer foreign key, "
-              + "pid integer primary key generated always as identity, "
-              + "caption varchar(180), "
-              + "imglocation varchar(52), "
-              + "date varchar(10), "
-              + ")");
+        try {
+            var stmt = connection.prepareStatement(
+                    "create table posts ("
+                            + "uid integer foreign key, "
+                            + "pid integer primary key generated always as identity, "
+                            + "caption varchar(180), "
+                            + "imglocation varchar(52), "
+                            + "date varchar(10), "
+                            + ")");
+        } catch(Exception e){
+               throw new RuntimeException(e);
+        }
     }
     public static void createPost(int userID, int postID,char text,char imageLocation,char date) throws SQLException{
                   //use post class / create post class instead of what is below 
