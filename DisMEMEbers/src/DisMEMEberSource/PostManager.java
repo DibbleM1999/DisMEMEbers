078e66267d35ce89f598d8ea8ceb548c8ab7be80
TehJellyLord <dionpcullum29@gmail.com>
2020-04-15 14:25:53 -0400

    Dion made some edits to the Post Manager to make it work better with the database singleton

--- DisMEMEbers/src/DisMEMEberSource/PostManager.java
+++ DisMEMEbers/src/DisMEMEberSource/PostManager.java
@@ -12,31 +12,29 @@ import java.sql.SQLException;
  */
 
 public class PostManager {
-    public static Connection connection;
     //public PostManager instance;
     //public static Post instance = new Post();
     public void PostManager(){
-        connection = setupTable();
+        setupTable();
     }
     
     private static final Connection JDBC_URL = DataBase.getInstance();
     
-    public static Connection setupTable() 
+    public static void setupTable() 
     {
-            try
-            {
-                connection.createStatement().execute("create table Post(userID int, postID int, text varchar(20), imageLocation varchar(20),date varchar(20)");
-                return connection;
-            } 
-            catch(SQLException e)
-            { 
-                throw new RuntimeException(e);
-            }
+            var stmt = JDBC_URL.prepareStatement(
+            "create table posts ("
+              + "uid integer foreign key, "
+              + "pid integer primary key generated always as identity, "
+              + "caption varchar(180), "
+              + "imglocation varchar(52), "
+              + "date varchar(10), "
+              + ")");
     }
     public static void createPost(int userID, int postID,char text,char imageLocation,char date) throws SQLException{
                   //use post class / create post class instead of what is below 
         connection.createStatement().execute(
-                "insert into Post values"+
+                "insert into post values"+
                 Post.instance.getUserID()+
                 Post.instance.getPostID()+
                 Post.instance.getText()+
