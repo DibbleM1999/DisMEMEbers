85b67383dc115787974c1d973a6acf7bd8049d3b
Fiery <jack@ponder-it.com>
2020-05-05 16:12:06 -0400

    login js  by Jack, mike,Hayden, and Alex D.

---
+++ DisMEMEbers/src/DisMEMEberSource/Login.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/*
+ * send input in to compare, and if logs in go to home page
+ */
+
+
+function NoAccount(){
+document.location = "Signup.html";
+}
