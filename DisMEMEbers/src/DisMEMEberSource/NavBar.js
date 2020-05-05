7e8f9000b89080a31bed6ef3d45c31fd1c8f3833
Fiery <jack@ponder-it.com>
2020-05-05 16:08:43 -0400

    Nav js  by Jack, mike,Hayden, and Alex D.

---
+++ DisMEMEbers/src/DisMEMEberSource/NavBar.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//if logged in/out
+//$("#Propic")
+//$(“.searchbar”)
+
+/*
+ * pull from database (img and username)
+ * searchbar
+ */
+    
